0f9b03f7e1aeb1d94fb4cdf4ba88f2e09a1c6721
roshan <roshan.prakash.r@gmail.com>
2014-09-24 17:48:51 +0100

    Using ID as a generic type

---
+++ Selectors/Selector.h
@@ -0,0 +1,15 @@
+//
+//  Selector.h
+//  Selectors
+//
+//  Created by roshan on 24/09/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface Selector:NSObject
+
+-(void) printValue: (id) obj;
+-(void) createSelector;
+
+@end
--- Selectors/main.m
+++ Selectors/main.m
@@ -7,11 +7,55 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Selector.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        //Example of using "id" type which a generic object
+
+        Selector *mySel = [[Selector alloc] init];
+        id dataStringObj = @"Roshan";
+        id dataIntObj = [NSNumber numberWithInt: 20];
+
+        [mySel printValue:dataStringObj];
+        [mySel printValue:dataIntObj];
+
+
+
+//        [mySel createSelector];
+//
+//        NSDate *now = [NSDate date];
+//
+//        NSString *date = [NSString stringWithFormat: @"The date today is %@", now];
+//        NSLog(@"%@", date);
+//
+//        NSHost *myComp = [[NSHost alloc] init];
+//        NSString *myC = [myComp localizedName];
+//        NSLog(@"%@", myC);
+//
+//
+//
+//        NSString *name = @"Prakash Roshan";
+//        NSLog(@"%@", name);
+//
+//        NSInteger charCount = [name length];
+//        NSLog(@"%li", charCount);
+//
+//        NSString *name1 = @"Roshan";
+//        NSString *name2 = @"Lekha";
+//
+//        if([name1 isEqualTo:name2])
+//        {
+//            NSLog(@"%@", @"true");
+//        }
+//        else
+//        {
+//            NSLog(@"%@", @"false");
+//        }
+
+        
+
     }
     return 0;
 }
